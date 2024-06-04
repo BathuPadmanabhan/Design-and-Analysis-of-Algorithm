418299e07d30a3d0ed55303da7e670733a435e19
Padmanabhan S <152837672+BathuPadmanabhan@users.noreply.github.com>
2024-06-04 13:38:03 +0530

    Create 10.RegularMatchingExpression.c

---
+++ 10.RegularMatchingExpression.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isMatch(char *s, char *p) {
+    if (*p == '\0') return *s == '\0';
+
+    bool firstMatch = (*s != '\0') && (*s == *p || *p == '.');
+
+    if (*(p + 1) == '*') {
+        return isMatch(s, p + 2) || (firstMatch && isMatch(s + 1, p));
+    } else {
+        return firstMatch && isMatch(s + 1, p + 1);
+    }
+}
+
+int main() {
+    char *s = "aa";
+    char *p = "a";
+    
+    bool result = isMatch(s, p);
+    
+    if (result) {
+        printf("Output: true\n");
+    } else {
+        printf("Output: false\n");
+    }
+    
+    return 0;
+}
