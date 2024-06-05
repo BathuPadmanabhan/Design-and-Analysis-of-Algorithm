00cb11b1e5f90deaba19182473032bdb01d6f8a7
Padmanabhan S <152837672+BathuPadmanabhan@users.noreply.github.com>
2024-06-05 13:40:40 +0530

    Upload 3.SumsUsingRecursiveAndNon-Recursive.c

--- Day_2/3.SumsUsingRecursiveAndNon-Recursive.c
+++ Day_2/3.SumsUsingRecursiveAndNon-Recursive.c
@@ -1,3 +1,4 @@
+//RECURSIVE
 #include<stdio.h>
 
 int fact(int num){
@@ -10,15 +11,29 @@ int fact(int num){
 
 int  main(){
      int ch, n;
-     scanf("%d",&ch);
-     printf("Enter 1 for recursive and @2 for NonRecursive");
-     
-     if(ch ==1){
+    
      scanf("%d",&n);
      int res = fact(n);
      printf("%d",res);
-     }
+
      
      return 0 ;
      
 }
+//NON_RECURSIVE
+/**
+
+int main() {
+    int n = 5;
+    int result = 1;
+
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+
+    printf("Factorial of %d is %d\n", n, result);
+
+    return 0;
+}
+
+**/
