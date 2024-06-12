d236a586a673178fa9b61efafc154c1102cf0bc6
Vimal-Mahendran <151828616+Vimal-Mahendran@users.noreply.github.com>
2024-06-12 12:53:12 +0530

    Update 3. MinimumDivisiontoCutaCircle.py

--- Assignment - 6/3. MinimumDivisiontoCutaCircle.py
+++ Assignment - 6/3. MinimumDivisiontoCutaCircle.py
@@ -1,8 +1,7 @@
-def minimum_cuts(n):
+def minimum_div(n):
     if n == 1:
         return 0
     return n
 
-# Test cases
-print(minimum_cuts(4))  # Output: 2
-print(minimum_cuts(3))  # Output: 3
+print(minimum_div(4))  
+print(minimum_div(3))  
