ff7ae871df5bbbd21d03dd9b3d16665b3fce62c4
Padmanabhan S <152837672+BathuPadmanabhan@users.noreply.github.com>
2024-06-13 12:38:38 +0530

    Add files via upload

---
+++ Day_8/4(2).Assignment.py
@@ -0,0 +1,10 @@
+def exhaustive_search(data, target):
+    for i in range(len(data)):
+        if data[i] == target:
+            return i
+    return -1
+
+data_list = [4, 7, 2, 9, 1, 5]
+target_value = 9
+result = exhaustive_search(data_list, target_value)
+print(f"Target value {target_value} found at index: {result}")
